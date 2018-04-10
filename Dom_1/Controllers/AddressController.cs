14da48d020bf269604801f53aca6b38b226f9093
Ivana Srbinoska <Ivana Srbinoska@MK_2010>
2018-04-11 01:21:44 +0200

    p

---
+++ Dom_1/Controllers/AddressController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dom_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dom_1.Controllers
+{
+    public class AddressController : Controller
+    {
+        //GET Address
+        public IActionResult Index()
+        {
+            Address adr = new Address
+            {
+                ID = 1,
+                FirstName = "John",
+                LastName = "Smith",
+                Address1 = "23 NW 13th St",
+                Address2 = "#134",
+                City = "Miami",
+                PostalCode = 22431
+            };
+            //ViewData["Message"] = adr;
+            return View(adr);
+        }
+    }
+}
---
+++ Dom_1/Models/Address .cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dom_1.Models
+{
+    public class Address
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public int PostalCode { get; set; }
+    }
+}
